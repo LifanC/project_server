93ec68eb6501e284117d2023f85d83068ad5a0e0
luke chen <bi1011105@gmail.com>
2023-08-21 02:32:30 +0800

    2023-08-23 luke 增加讀取CSV顯示在畫面

--- src/main/java/com/project/server/Controller/SetUpController.java
+++ src/main/java/com/project/server/Controller/SetUpController.java
@@ -3,14 +3,13 @@ package com.project.server.Controller;
 import com.project.server.Model.TableName;
 import com.project.server.Service.SetUpService;
 import jakarta.annotation.Resource;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.*;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -26,13 +25,18 @@ public class SetUpController {
     }
 
     @GetMapping("readCsv")
-    public List<String[]> readCsv() {
+    public List<Object> readCsv() {
+        List<Object> list = new ArrayList<>();
 
         try {
             String filePath = "D:\\project3.0\\file_csv\\fileOne.csv";
-            return setUpService.readCsvData(filePath);
+            List<String[]> str = setUpService.readCsvData(filePath);
+            list.add(str);
+            return list;
         } catch (IOException e) {
-            return null;
+            list.add("error");
+            list.add(String.valueOf(e));
+            return list;
         }
 
     }
