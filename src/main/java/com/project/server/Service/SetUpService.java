6d60663400ab7192bf198fa3839e392c502cda5c
luke chen <bi1011105@gmail.com>
2023-10-29 21:32:23 +0800

    2023-10-29 Luke修改專案

--- src/main/java/com/project/server/Service/SetUpService.java
+++
@@ -1,9 +0,0 @@
-package com.project.server.Service;
-
-import java.io.IOException;
-import java.util.List;
-
-public interface SetUpService {
-
-    List<String[]> readCsvData(String filePath) throws IOException;
-}
