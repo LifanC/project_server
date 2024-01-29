e93f53e51fb7a587d044fb612b659388374576aa
luke chen <bi1011105@gmail.com>
2024-01-30 00:46:04 +0800

    2024-01-30

--- src/main/java/com/project/server/Common/Ireport.java
+++ src/main/java/com/project/server/Common/Ireport.java
@@ -4,6 +4,7 @@ import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.engine.design.JasperDesign;
 import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.view.JasperViewer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,20 +14,24 @@ import java.util.*;
 public class Ireport {
     private static final Logger logger = LoggerFactory.getLogger(Ireport.class);
 
-    public static void folderMkdirsFunction(String pdf){
+    public static void folderMkdirsFunction(String pdf) {
         File folder = new File(pdf);
         if (!folder.exists() && !folder.isDirectory()) {
             if (folder.mkdirs()) {
-                logger.info("PdfReport: {}", "目錄創建成功");
+                logger.info("PdfReport: {},{}", "目錄創建成功", pdf);
+            } else {
+                logger.info("PdfReport: {},{}", "目錄已存在", pdf);
             }
         }
     }
 
-    public static void exportReportFunctionPDF(List<?> dataList, String iReportFilePath, Map<String, Object> paramsMap, String pdfPath) throws JRException {
-        JRDataSource jrDataSource = new JRBeanCollectionDataSource(dataList);
+    public static void exportReportFunctionPDF(
+            List<Map<String, Object>> dataList, String iReportFilePath, Map<String, Object> header, String pdfPath
+    ) throws JRException {
         JasperDesign jasperDesign = JRXmlLoader.load(iReportFilePath);
         JasperReport jasperReport = JasperCompileManager.compileReport(jasperDesign);
-        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, paramsMap, jrDataSource);
+        JRDataSource jrDataSource = new JRBeanCollectionDataSource(dataList);
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, header, jrDataSource);
         JasperExportManager.exportReportToPdfFile(jasperPrint, pdfPath);
         logger.info("exportReportFunction: {}", "PDF輸出成功");
     }
--- src/main/java/com/project/server/Controller/W001Controller.java
+++ src/main/java/com/project/server/Controller/W001Controller.java
@@ -5,23 +5,22 @@ import com.project.server.Common.Ireport;
 import com.project.server.Model.GoW001;
 import com.project.server.Service.GoW001.W001Service;
 import jakarta.annotation.Resource;
-import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/W001")
 public class W001Controller {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-    private final String rootDirectory = "D:\\project3.0\\";
+    private static final String rootDirectory = "D:\\project3.0\\";
     private final Gson gson = new Gson();
     @Resource
     private W001Service w001Service;
@@ -75,49 +74,60 @@ public class W001Controller {
         return w001Service.goW001Search(goW001_datePickers_array);
     }
 
+    ArrayList<Map<String, Object>> tert;
     @PostMapping("/goW001printIreport")
     public String goW001printIreport(@RequestBody Map<String, ArrayList<Map<String, Object>>> params) throws JRException {
         ArrayList<Map<String, Object>> list12 = new ArrayList<>();
         ArrayList<String> pdfPathList = new ArrayList<>();
         params.values().forEach(list12::addAll);
         Collections.reverse(list12);
-        System.out.println(list12);
+        tert = list12;
         final String W001Name = "W001";
-        String iReportFile = rootDirectory + "server\\ierport\\" + W001Name + ".jrxml";
         String pdfPath = rootDirectory + W001Name + "reportpdf\\";
+        String iReportFilePdfPath = rootDirectory + "server\\ierport\\";
+        String iReportFile = iReportFilePdfPath + W001Name + ".jrxml";
         if (!CollectionUtils.isEmpty(list12)) {
-//            Ireport.folderMkdirsFunction(pdfPath);
+            Ireport.folderMkdirsFunction(pdfPath);
+            Ireport.folderMkdirsFunction(iReportFilePdfPath);
             String pdfName = W001Name + RandomUniqueString();
+            String folderNames_pdf = pdfPath + pdfName;
             //新增Ireport的表頭
             Map<String, Object> header = new HashMap<>();
-
+            header.put("title", "W0011 Report");
+            header.put("W001Name", W001Name);
+            header.put("pdfName", pdfName);
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            header.put("toDay", simpleDateFormat.format(new Date()));
             //輸出PDF
-//            Ireport.exportReportFunctionPDF(list12, iReportFile, header, pdfPath + pdfName + ".pdf");
+            Ireport.exportReportFunctionPDF(list12, iReportFile, header, folderNames_pdf + ".pdf");
             pdfPathList.add("Success");
-            pdfPathList.add(pdfPath);
-            logger.info("W001報表資料數量: {}",list12.size());
+            pdfPathList.add(folderNames_pdf + ".pdf");
+            logger.info("W001報表資料數量: {}", list12.size());
+        } else {
+            pdfPathList.add("Fail");
+            pdfPathList.add("");
+            logger.info("W001報表資料數量: {}", 0);
         }
         return gson.toJson(pdfPathList);
     }
 
-    private String RandomUniqueString(){
+    private String RandomUniqueString() {
         // 產生所有可能的英文字符和數字
-        String allCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        String allCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         // 將所有字符放入ArrayList中，以便洗牌
         ArrayList<Character> charList = new ArrayList<>();
-        for(char c : allCharacters.toCharArray()){
+        for (char c : allCharacters.toCharArray()) {
             charList.add(c);
         }
         // 隨機排列字符
         Collections.shuffle(charList);
         // 從ArrayList中選取您需要的字符數量
-        int desiredLength = 30; // 設定所需長度
+        int desiredLength = 5; // 設定所需長度
         StringBuilder randomString = new StringBuilder();
         for (int i = 0; i < desiredLength; i++) {
             randomString.append(charList.get(i));
         }
         return randomString.toString();
     }
-
 
 }
