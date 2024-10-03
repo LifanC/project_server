21ccb1c51f24d496671e0cc352777eb914a53d38
luke chen <bi1011105@gmail.com>
2024-10-04 00:33:15 +0800

    2024-10-04 Luke

--- src/main/java/com/project/server/Controller/W005Controller.java
+++ src/main/java/com/project/server/Controller/W005Controller.java
@@ -1,24 +1,61 @@
 package com.project.server.Controller;
 
 //import com.google.gson.Gson;
+
+import com.project.server.Entity.GoW005Bean;
+import com.project.server.Service.GoW005.W005Service;
+import io.micrometer.common.util.StringUtils;
+import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/W005")
 public class W005Controller {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-//    private final Gson gson = new Gson();
+    //    private final Gson gson = new Gson();
+    private final static String path =
+            "D:" + File.separator + "project3.0" + File.separator + "W005";
+
+    @Resource
+    private W005Service w005Service;
 
     @GetMapping("/goW005")
     public String goW005() {
         logger.info("Start goW005");
-        return "JokeAPI";
+        return "讀取資料";
+    }
+
+    @GetMapping("/W005UrlDefault")
+    public String W005UrlDefault(
+            @RequestParam String f_name,
+            @RequestParam String number,
+            @RequestParam String fileName
+    ) {
+        logger.info("Start W005UrlDefault: {},{},{}", f_name, number, fileName);
+        if (StringUtils.isBlank(f_name) && StringUtils.isBlank(number)) {
+            return "";
+        }
+        return path + File.separator + fileName;
+    }
+
+    @PostMapping("/goW005methodSelect")
+    public Map<String, Object> goW005methodSelect(@RequestBody Map<String, GoW005Bean> params) {
+        GoW005Bean goW005 = params.get("GoW005");
+        logger.info("Start goW005: {}", goW005);
+        if (StringUtils.isBlank(goW005.getF_name()) && StringUtils.isBlank(goW005.getNumber())) {
+            return null;
+        }
+        Map<String, Object> returnMap = new HashMap<>();
+        returnMap.put("data", w005Service.goW005methodSelect(goW005.getFileName(), path));
+        return returnMap;
     }
 
 }
